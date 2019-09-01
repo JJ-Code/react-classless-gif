995eeb5c5dfb7e0114960a6cda3895da82e8d1ce
Jaymee Liu <{jaymeeliu@gmail.com}>
2019-08-31 21:10:26 -0700

    hook  version

--- src/Giphy.js
+++ src/Giphy.js
@@ -1,140 +1,141 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import MakeCard from "./MakeCard";
 
 
 
-class Giphy extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            computerScore: Math.floor(Math.random() * 102) + 19,
-            winScore: 0,
-            lossScore: 0,
-            userScore: 0
-        };
-        this.click = this.click.bind(this);
-        this.gifCard = this.gifCard.bind(this);
-        this.gamePlayHandleClick = this.gamePlayHandleClick.bind(this);
+const Giphy = (props) => {
+  const [gameData, setData] = useState({
+    computerScore: Math.floor(Math.random() * 102) + 19,
+    userScore: 0
+  })
+
+  const [gameHistory, setGameHistory] = useState({
+    winScore: 0,
+    lossScore: 0,
+  })
+
+  console.log(gameData.computerScore)
+
+
+  const win = () => {
+    setGameHistory(st => ({
+      winScore: st.winScore += 1,
+      lossScore: st.lossScore
+    }))
+    alert("You win!");
+    resetGiphy();
+    return () => props.restGame();
+  }
+
+  const loss = () => {
+    setGameHistory(st => ({
+      winScore: st.winScore,
+      lossScore: st.lossScore += 1
+    }))
+    alert("You did not win, loser!");
+    resetGiphy();
+    return () => props.restGame();
+  }
+
+  const resetGiphy = () => {
+    setData({
+      userScore: 0,
+      computerScore: Math.floor(Math.random() * 102) + 19,
+    })
+  };
+
+
+  const gifCard = () => {
+    console.log(props.gameCard)
+    if (props.gameCard.length !== 8) {
+      return (<h2>Loading...</h2>)
     }
 
-    click() {
-        console.log(this.props.gameCard[0])
+    else {
+
+     return props.gameCard.map(card => (
+        <MakeCard key={card.id}
+          id={card.id}
+          url={card.url}
+          pts={card.pts}
+          gamePlayHandleClick={gamePlayHandleClick} />
+      ))
 
     }
-    
-    
-    win() {
-        this.setState(st => ({
-            winScore: st.winScore += 1
-        }))
-        alert("You win!");
-        this.resetGiphy();
-        return () => this.props.restGame();
+  }
+
+  const gamePlayHandleClick = (evt) => {
+    // console.log(evt)
+    // console.log(evt.target.dataset.id);
+    const idPic = evt.target.dataset.id; //mouse object location in array
+    const foundPic = props.gameCard.findIndex(gifPic => {
+      console.log(gifPic.id === idPic)
+      return gifPic.id === idPic
+    });
+    setData(st => ({
+      userScore: st.userScore += props.gameCard[foundPic].pts,
+      computerScore: st.computerScore
+    }))
+    //(this.state.userScore > this.state.computerScore) ? this.state.loss() : this.state.win();
+    if (gameData.userScore === gameData.computerScore) {
+      win();
+    } else if (gameData.userScore > gameData.computerScore) {
+      loss();
     }
-
-    loss() {
-        this.setState(st => ({
-            lossScore: st.lossScore += 1
-        }))
-        alert("You did not win, loser!");
-        this.resetGiphy();
-        return () => this.props.restGame();
-    }
-
-    resetGiphy() {
-        this.setState({
-            userScore: 0,
-            computerScore: Math.floor(Math.random() * 102) + 19,
-        })
-    };
+    console.log(gameData);
+    console.log(gameData);
+  }
 
 
-    gifCard() {
-        return this.props.gameCard.map(card => (
-            <MakeCard
-                key={card.id}
-                id={card.id}
-                url={card.url}
-                pts={card.pts}
-                gamePlayHandleClick={this.gamePlayHandleClick}
 
-            />
-        ))
-    }
+  return (
+    <div className="container" ref={props.myDivToFocus}>
+      <section>
+        <div className="content" id="main-game">
+          <div className="row">
+            <h1>Step 2 - Click on giphy to match SCORE to WIN! </h1> <br /><br />
+            <div className="col-md-4">
+              <h2>Try to match</h2>
+              <h2 className="computer-score"> {gameData.computerScore}</h2>
 
-    gamePlayHandleClick(evt) {
-        // console.log(evt)
-        // console.log(evt.target.dataset.id);
-        const idPic = evt.target.dataset.id; //mouse object location in array
-        const foundPic = this.props.gameCard.findIndex(gifPic => {
-            console.log(gifPic.id === idPic)
-            return gifPic.id === idPic
-        });
-        this.setState(st => ({
-            userScore: st.userScore += this.props.gameCard[foundPic].pts
-        }))
-            //(this.state.userScore > this.state.computerScore) ? this.state.loss() : this.state.win();
-        if (this.state.userScore === this.state.computerScore) {
-            this.win();
-        } else if (this.state.userScore > this.state.computerScore) {
-            this.loss();
-        }
-        console.log(this.state.userScore);
-    }
+              <div></div>
+            </div>
 
-    render() {
+            <div className="col-md-4">
+              <h2>Your current score: </h2>
+              <h2 className="user-score">{gameData.userScore}</h2>
+            </div>
+            <div className="col-md-4">
+              <h2>Score Tracker</h2>
+              <h3>Wins: </h3>
+              <h3 className="win-score"> {gameHistory.winScore} </h3>
 
+              <h3>Losses: </h3>
+              <h3 className="loss-score">{gameHistory.lossScore} </h3>
+            </div>
+          </div>
 
-        return (
-            <div className="container" ref={this.props.myDivToFocus}>
-                <section>
-                    <div className="content" id="main-game">
-                        <div className="row">
-                            <h1>Step 2 - Click on giphy to match SCORE to WIN! </h1> <br /><br />
-                            <div className="col-md-4">
-                                <h2>Try to match</h2>
-                                <h2 className="computer-score"> {this.state.computerScore}</h2>
+          {/* <!-- GIF RESULTS GAME--> */}
+          <div className="row">
 
-                                <div></div>
-                            </div>
+            <div id="giphy-view">
+              {gifCard()}
+            </div>
 
-                            <div className="col-md-4">
-                                <h2>Your current score: </h2>
-                                <h2 className="user-score">{this.state.userScore}</h2>
-                            </div>
-                            <div className="col-md-4">
-                                <h2>Score Tracker</h2>
-                                <h3>Wins: </h3>
-                                <h3 className="win-score"> {this.state.winScore} </h3>
-
-                                <h3>Losses: </h3>
-                                <h3 className="loss-score">{this.state.lossScore} </h3>
-                            </div>
-                        </div>
-
-                        {/* <!-- GIF RESULTS GAME--> */}
-                        <div className="row">
-
-                            <div id="giphy-view">
-                                {(this.props.loading === false) ? <h2>Loading...</h2> : this.gifCard()}
+          </div>
 
 
-                            </div>
+        </div>
+      </section>
+    </div>//end of 
 
-                        </div>
+  )
 
-
-                    </div>
-                </section>
-            </div>//end of 
-
-        )
-
-
-    }
 
 }//end of giphy 
 
 
-export default Giphy; 
+
+
+export default Giphy;
