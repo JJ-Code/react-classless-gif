1f619873a9ae1a7123cbb4cbd6ba40b004078ded
Jaymee Liu <{jaymeeliu@gmail.com}>
2019-09-02 18:34:06 -0700

    update context2

--- src/hooks/useGameState.js
+++ src/hooks/useGameState.js
@@ -1,36 +1,21 @@
-// import {
-//     useState
-// } from "react";
+import {
+    useState
+} from "react";
 
-// export default initialTodos => {
-//     const [gameData, setGameData] = useState(initialTodos);
-//     return {
-//         gameData,
-//         reset: resetScore => {
-//             setGameData(setGameData)
-//         },
-//         addUserScore: score => {
-//             setGameData(initalState => {
-//                 initalState += score
-//             });
-//         },
-//         toggleTodo: todoId => {
-//             const updatedTodos = todos.map(todo =>
-//                 todo.id === todoId ? {
-//                     ...todo,
-//                     completed: !todo.completed
-//                 } : todo
-//             );
-//             setTodos(updatedTodos);
-//         },
-//         editTodo: (todoId, newTask) => {
-//             const updatedTodos = todos.map(todo =>
-//                 todo.id === todoId ? {
-//                     ...todo,
-//                     task: newTask
-//                 } : todo
-//             );
-//             setTodos(updatedTodos);
-//         }
-//     };
-// };
+export default initialTodos => {
+    const [gameData, setGameData] = useState(initialTodos);
+    return {
+        gameData,
+        reset: resetScore => {
+            setGameData(setGameData)
+        },
+        addUserScore: score => {
+            setGameData(initalState => {
+                initalState += score
+            });
+        },
+        updateState: state => {
+            setGameData(state)
+        }
+    };
+};
